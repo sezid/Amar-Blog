8038a482446a508710a2fc910f77b2b7a95721bf
sezid <bradsajid@gmail.com>
2023-12-24 14:50:01 +0600

    first part

---
+++ src/Home.js
@@ -0,0 +1,28 @@
+import {useState, useEffect} from 'react';
+import BlogList from './BlogList';
+
+const Home = () => {
+
+    const [blogs, setBlogs]=useState(null);
+
+
+    useEffect(()=>{
+        fetch('http://localhost:8000/blogs')
+            .then(res => {
+                return res.json()
+            })
+            .then(data =>{
+                console.log(data)
+                setBlogs(data);
+            });
+    },[]);
+
+    return (  
+        <div className="home">
+            {blogs && <BlogList blogs={blogs} title="All blogs!!" />}
+
+        </div>
+    );
+}
+ 
+export default Home;
